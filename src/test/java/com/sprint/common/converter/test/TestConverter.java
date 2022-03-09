54a755fa97a53e9af983630ee31e5d83e5d6d5f1
lihongfeng <hongfeng.li@bianlifeng.com>
2022-03-09 19:19:23 +0800

    use cache

--- src/test/java/com/sprint/common/converter/test/TestConverter.java
+++ src/test/java/com/sprint/common/converter/test/TestConverter.java
@@ -60,7 +60,7 @@ public class TestConverter {
         inner1.setInner("100000");
         bean1.setListList(Collections.singletonList(Collections.singletonList(inner1)));
         bean1.setHouse(Collections.singletonList("yanchengyuan"));
-        bean1.setArray("[\"1231231\"]");
+        //bean1.setArray("[\"1231231\"]");
 
         TypeBean2<List<List<Integer>>> bean2 = AnyConverter.convert(bean1,
                 new TypeReference<TypeBean2<List<List<Integer>>>>() {
@@ -68,7 +68,7 @@ public class TestConverter {
 
         long tms = System.currentTimeMillis();
 
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 100000; i++) {
             TypeBean2<List<List<Integer>>> item = AnyConverter.convert(bean1,
                     new TypeReference<TypeBean2<List<List<Integer>>>>() {
                     });
