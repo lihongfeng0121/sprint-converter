7261a82c375c664d3b725c3f85efe7db4968aebf
lihongfeng <hongfeng.li@bianlifeng.com>
2022-09-03 22:07:30 +0800

    add boolean type converter

--- src/main/java/com/sprint/common/converter/exception/package-info.java
+++ src/main/java/com/sprint/common/converter/exception/package-info.java
@@ -1,7 +1,4 @@
 /**
  * 异常类
- *
- * @author hongfeng.li
- * @since 2022/9/3
  */
 package com.sprint.common.converter.exception;
--- src/main/java/com/sprint/common/converter/util/package-info.java
+++ src/main/java/com/sprint/common/converter/util/package-info.java
@@ -1,7 +1,4 @@
 /**
  * 工具类
- *
- * @author hongfeng.li
- * @since 2022/9/3
  */
 package com.sprint.common.converter.util;
